49460ec2515597c5be7861b98872fb81f74d9f0f
Dylan Drumm <112513127+ddrumm1CSC@users.noreply.github.com>
2022-10-28 15:26:14 -0400

    Registration Page Prototype
    Text fields to input email/password, no functionality just placeholder.

--- i-Schedule-app/i_schedule/lib/screens/RegisterPage.dart
+++ i-Schedule-app/i_schedule/lib/screens/RegisterPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
+
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
@@ -9,9 +11,84 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text("Register Page"),
-        ));
+      backgroundColor: const Color.fromARGB(255, 222, 222, 228),
+      appBar: AppBar(
+        title: const Text("iSchedule - Registration"),
+        backgroundColor: const Color.fromARGB(255, 25, 97, 156),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter email ID'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter password'),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+                height: 40,
+                width: 180,
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => RegisterPage()));
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80.0)),
+                  padding: const EdgeInsets.all(0.0),
+                  child: Ink(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment(0.8, 1),
+                        colors: <Color>[
+                          Color.fromARGB(255, 9, 73, 126),
+                          Color.fromARGB(255, 41, 132, 206)
+                        ],
+                        tileMode: TileMode.mirror,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: Container(
+                      constraints:
+                          const BoxConstraints(minWidth: 90.0, minHeight: 40.0),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 222, 222, 228),
+                            fontSize: 20),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
